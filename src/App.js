f687d3bd4c1ef65661f352aed87db7584c17453f
FAWAd7998 <fawad7998@gmail.com>
2023-01-30 17:47:43 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,272 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./index.js";
 
-function App(props) {
+function App() {
   return (
-   <div>
-    <h1>{props.name}</h1>
-   </div>
+    <>
+      <body>
+        <section id="nav_back_color">
+          <section id="navbar">
+            <div className="site-name">SITE NAME</div>
+            <section id="nav-item">
+              <section id="navbar-1">
+                <div className="nav-items">
+                  <a href="/"> Home</a>
+                  {/* <!-- fontawesome icon -->
+							<!-- <i className="fa-regular fa-angle-right"></i> --> */}
+                </div>
+                <div className="nav-items">
+                  <a href="/"> Landings</a>
+                </div>
+                <div className="nav-items">
+                  <a href="/"> Pages</a>
+                </div>
+                <div className="nav-items">
+                  <a href="/"> Documentation</a>
+                </div>
+              </section>
+              <div className="nav-items" id="button-1">
+                <a href="/">Buy Now</a>
+              </div>
+            </section>
+          </section>
+        </section>
+
+        {/* <!-- SECTION 02 --> */}
+        <section id="section-02">
+          <section>
+            <div id="bold-text">
+              Build your next project faster with SB UI Kit Pro
+            </div>
+            <div id="sub-text">
+              Welcome to SB UI Kit Pro ,a toolkit for building beautiful web
+              interfaces, created by the development team at start Bootstrap
+            </div>
+            <section id="span">
+              <div id="section-02-buttom">
+                <a id="anchor" href="/">
+                  View Pages
+                </a>
+              </div>
+              <div id="doc">
+                <a id="anchor" href="/">
+                  Documentation
+                </a>
+              </div>
+            </section>
+          </section>
+          <img id="img-one" src="./one.png" alt="" />
+        </section>
+
+        {/* <!-- section 03 --> */}
+        <section id="section-03">
+          <section id="first-box">
+            <section id="item-box">
+              <div id="rounded">
+                <i className="bi bi-database"></i>
+              </div>
+            </section>
+            <div id="box-text">Build for developers</div>
+            <div id="box-sub-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Recusandae odit delectus sed architecto nihil, ipsam qui vitae
+              molestiae consequuntur laborum error now day's.
+            </div>
+          </section>
+          <section id="first-box">
+            <section id="item-box">
+              <div id="rounded">
+                <i className="bi bi-phone"></i>
+              </div>
+            </section>
+            <div id="box-text">Modern responsive design</div>
+            <div id="box-sub-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Recusandae odit delectus sed architecto nihil, ipsam qui vitae
+              molestiae consequuntur laborum error now day's.
+            </div>
+          </section>
+          <section id="first-box">
+            <section id="item-box">
+              <div id="rounded">
+                <i className="bi bi-database"></i>
+              </div>
+            </section>
+            <div id="box-text">Complete documentation</div>
+            <div id="box-sub-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Recusandae odit delectus sed architecto nihil, ipsam qui vitae
+              molestiae consequuntur laborum error now day's.
+            </div>
+          </section>
+        </section>
+
+        {/* <!-- js section --> */}
+        <section id="section-dummy"></section>
+        {/* <!-- ............ --> */}
+
+        {/* <!-- section 04 --> */}
+        <section id="nav-back-color-02">
+          <section id="sec-items">
+            <img id="img-two" src="./two.png" alt="" />
+            <section id="sec-text">
+              <div id="text-one">Here's What You Get</div>
+              <div id="text-two">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                aut labore minima consectetur cum, adipisci delectus at. Esse,
+                repudiandae, atque enim voluptas necessitatibus inventore
+                commodi suscipit odit architecto consequatur doloremque nam fuga
+                quidem mollitia sed obcaecati molestias unde a eius.
+              </div>
+
+              {/* <!-- sec-boss --> */}
+              <section id="sec-main-boss">
+                <section id="sec-main">
+                  <section id="sec-01">
+                    <section id="mini-texts">
+                      <div className="text-03">landing Pages</div>
+                      <div className="text-04">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Veritatis laborum nam laboriosam quae odio.
+                      </div>
+                      <div id="button-learn">
+                        <a href="/"> Learn more</a>
+                      </div>
+                    </section>
+                  </section>
+                  <section id="sec-02">
+                    <section id="mini-texts">
+                      <div className="text-03">Layouts Pages</div>
+                      <div className="text-04">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Veritatis laborum nam laboriosam quae odio.
+                      </div>
+                      <div id="button-learn">
+                        <a href="/"> Learn more</a>
+                      </div>
+                    </section>
+                  </section>
+                </section>
+                <section id="sec-main-02">
+                  <section id="sec-01">
+                    <section id="mini-texts">
+                      <div className="text-03">Page Examples</div>
+                      <div className="text-04">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Veritatis laborum nam laboriosam quae odio.
+                      </div>
+                      <div id="button-learn">
+                        <a href="/"> Learn more</a>
+                      </div>
+                    </section>
+                  </section>
+                  <section id="sec-02">
+                    <section id="mini-texts">
+                      <div className="text-03">Modular Sections</div>
+                      <div className="text-04">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Veritatis laborum nam laboriosam quae odio.
+                      </div>
+                      <div id="button-learn">
+                        <a href="/"> Learn more</a>
+                      </div>
+                    </section>
+                  </section>
+                </section>
+              </section>
+            </section>
+          </section>
+          <section id="nav-color-03"></section>
+          <section className="sec-end">
+            <div>Simple Pricing</div>
+          </section>
+          <section className="sec-end-02">
+            <div>Easy pricing means no surprises.</div>
+          </section>
+        </section>
+
+        {/* <!-- section 05 --> */}
+
+        <section id="section-05">
+          <section id="nav-color-04"></section>
+          <section id="sec-05-back"></section>
+          {/* <!-- box --> */}
+          <section id="sec-05-box">
+            <div id="box-01">
+              <div id="tag-free">Free</div>
+              <div id="num">
+                <span>$</span>
+                <span> 0/mo</span>
+              </div>
+              <div id="items">
+                <div className="box-items">1 user</div>
+                <div className="box-items">Community support</div>
+                <div className="box-items">Style customizer</div>
+                <div className="box-items">Custom components</div>
+                <div className="box-items">Expanded utilities</div>
+                <div className="box-items">Third-party integration</div>
+                <div className="box-items">Layout options</div>
+              </div>
+            </div>
+          </section>
+          <section id="sec-05-box-02"></section>
+
+          {/* <!-- section 06 --> */}
+          <section id="nav-color-05"></section>
+
+          {/* <!-- what is sb kit --> */}
+          <section id="flex">
+            <div id="circle">
+              <i className="bi bi-question-lg"></i>
+            </div>
+            <section id="circle-item">
+              <div>What Is SB UI Kit Pro?</div>
+              <div id="div-2">
+                SB Ui Kit Pro is a fully coded, responsive, Bootstrap based UI
+                toolkit for developers.
+              </div>
+            </section>
+          </section>
+          {/* <!-- ......................... --> */}
+          <section className="max-content">
+            <div id="get">Get started</div>
+          </section>
+          <section className="max-content" id="max-content-id">
+            <div id="text-dec">Save Time With SB UI Kit Pro</div>
+          </section>
+          <section className="max-content" id="max-content-id-02">
+            <div id="text-dec-02">
+              start Bootstrap premium UI Kit beautifully and intuitively extends
+              the Bootstraps frameworks making it easy to build your next
+              project!
+            </div>
+          </section>
+          <section id="button-05">
+            <div id="buy-now">
+              <a href="/">Buy Now</a>
+            </div>
+            {/* <!-- <div className="nav-items" id="button-1"><a href="/">Buy Now</a></div> --> */}
+          </section>
+        </section>
+
+        {/* <!-- section 07 --> */}
+
+        <section id="section-07">
+          <section id="sec-07">
+            <section id="sec-item-01">
+              <div id="google">Google</div>
+              <div>
+                "Stripe Billingenabled us to Launch our subscription product.
+                eero plus, on schedule and with billing fully integrated. "
+              </div>
+              <div> Mark Sirglock</div>
+              <div>Head of Business Operations</div>
+            </section>
+          </section>
+        </section>
+      </body>
+    </>
   );
 }
 
