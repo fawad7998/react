fe64beed809d1d434f9595792a48800365752ad9
FAWAd7998 <fawad7998@gmail.com>
2023-02-08 22:45:50 +0500

    first

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import FirstPage from "./sections/Firstpage.js";
 import SecondPage from "./sections/SecondPage.js";
 import Thirdpage from "./sections/Thirdpage.js";
 import { Routes, Route } from "react-router-dom";
+import Textform from "./sections/Textform.js";
 
 function App() {
   return (
     <>
         <Routes>
-          <Route path="/home" element={<FirstPage />} />
+          <Route path="" element={<FirstPage />} />
           <Route path="/second" element={<SecondPage/>} />
           <Route path="/third" element={<Thirdpage/>} />
+          <Route path="/form" element={<Textform/>} />
         </Routes>
 
     </>
--- src/sections/Textform.js
+++ src/sections/Textform.js
@@ -1,11 +1,55 @@
-const Textform = ()=>{
-<>
-<div class="mb-3">
-<label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-</>
-}
-   
-   
+import Navbar from "../components/Navbar";
+import React, { useState } from "react";
+
+const Textform = () => {
+  // setText = "This is a Text 02";
+  
+  function upClick() {
+    console.log("clicked");
+    let newtext = text.toUpperCase();
+    setText(newtext);
+  }
+  function downClick() {
+    console.log("clicked");
+    let newtext = text.toLowerCase();
+    setText(newtext);
+  }
+  
+  function onchange(event) {
+    // console.log("on changed")  
+    setText(event.target.value);
+  }
+  
+  const [text,setText] = useState("This is a Text");
+ 
+  return (
+    <>
+      {/* <Navbar /> */}
+      <div className="container">
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">
+            TextArea
+          </label>
+          <textarea
+            value={text}
+            onChange={onchange}
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="5"
+          ></textarea>
+        </div>  
+        <button onClick={upClick} className="btn btn-primary  ">Upper Case</button>
+        <button onClick={downClick} className="btn btn-primary mx-2">Lower Case</button>
+      </div>
+      <div className="container my-3">
+        <h1>INFO</h1>
+        <p>{text.split(" ").length}words ,{text.length}characters</p>
+        <p>{0.008 * text.split(" ").length}words per minute</p>
+
+      </div>
+    </>
+  );
+};
 export default Textform;
+
+// (Event) => event.target.value (now we can type in our Textfield ny using this)
